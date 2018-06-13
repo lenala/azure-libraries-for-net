3fdb69297818238b7970d7c93a4711d69ea0c50e
lenala <lena.lakhno@gmail.com>
2018-06-13 11:40:17 -0700

    test

--- ci/create_image.sh
+++ ci/create_image.sh
@@ -12,13 +12,3 @@ function title {
 title 'Generating tasks list'
 ./ci/get_tasks_list.sh
 
-title 'Building docker image'
-image=azuresdk-net:net-$TRAVIS_BUILD_NUMBER
-docker build -t $image -f ci/Dockerfile .
-
-title 'Pushing docker image'
-docker login azureclidev.azurecr.io -u $AZURESDKDEV_ACR_SP_USERNAME -p $AZURESDK_ACR_SP_PASSWORD
-docker tag azuresdk-net:net-$TRAVIS_BUILD_NUMBER azureclidev.azurecr.io/azuresdk-net:net-$TRAVIS_BUILD_NUMBER
-docker tag azuresdk-net:net-$TRAVIS_BUILD_NUMBER azureclidev.azurecr.io/azuresdk-net:net-latest
-docker push azureclidev.azurecr.io/azuresdk-net:net-$TRAVIS_BUILD_NUMBER
-docker push azureclidev.azurecr.io/azuresdk-net:net-latest
